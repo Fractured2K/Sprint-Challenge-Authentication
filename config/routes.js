828eb4f3956ddfc73e8f30e2bbece7ccad0f839c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-05 08:25:29 -0700

    Handle login controller

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
+const Users = require('../models/Users');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -26,8 +27,25 @@ async function register(req, res) {
 	}
 }
 
-function login(req, res) {
-	// implement user login
+async function login(req, res) {
+	try {
+		let { username, password } = req.body;
+
+		const user = await Users.findBy({ username }).first();
+
+		if (user && bcrypt.compareSync(password, user.password)) {
+			const token = generateToken(user);
+			return res.status(200).json(token);
+		}
+
+		return res
+			.status(401)
+			.json({ message: 'Sorry, incorrect username or password' });
+	} catch (err) {
+		res.status(500).json({
+			message: 'Sorry, but there was an error while trying to log in '
+		});
+	}
 }
 
 function getJokes(req, res) {
