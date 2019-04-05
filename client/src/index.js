34fd48e62e45a8d29c006a186def50aad14d15a2
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-05 09:08:44 -0700

    Wrap app with router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppWithRouter = withRouter(App);
+
+ReactDOM.render(<AppWithRouter />, document.getElementById('root'));
