2c2464ec6246cf8ee5bd01027b8d6e4a56709fe8
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-05 10:05:34 -0700

    Render jokes

---
+++ client/src/components/Joke.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Joke = props => {
+	return (
+		<div>
+			<p>{props.joke}</p>
+		</div>
+	);
+};
+
+export default Joke;
--- client/src/components/JokeList.js
+++ client/src/components/JokeList.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Joke from './Joke';
 import isAuthenticated from '../components/Auth/isAuthenticated';
+
 class JokeList extends Component {
+	state = {
+		jokes: []
+	};
+
 	render() {
 		return (
 			<div>
-				<h1>hello</h1>
+				{this.state.jokes.map(joke => (
+					<Joke key={joke.id} {...joke} />
+				))}
 			</div>
 		);
 	}
+
+	componentDidMount = () => {
+		axios
+			.get('/jokes')
+			.then(res => this.setState({ jokes: res.data }))
+			.catch(err => console.log(err));
+	};
 }
 
 export default isAuthenticated(JokeList);
