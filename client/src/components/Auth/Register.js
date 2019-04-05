dd25872f87a1017580dc1086a81f9ae2ae49efa3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-05 09:21:40 -0700

    Create form for register component

---
+++ client/src/components/Auth/Register.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.onSubmit}>
+					<input
+						type="text"
+						name="username"
+						placeholder="username"
+						value={this.state.username}
+						onChange={this.handleChanges}
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="password"
+						value={this.state.password}
+						onChange={this.handleChanges}
+					/>
+					<button>Register</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Register;
