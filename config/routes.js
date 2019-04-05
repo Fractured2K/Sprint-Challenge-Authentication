256c047e8a61a3470d3841d05ae52e979ae02466
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-05 08:16:49 -0700

    Handle register controller

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
+const User = require('../models/User');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -8,8 +10,20 @@ module.exports = server => {
 	server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
-	// implement user registration
+async function register(req, res) {
+	try {
+		let user = req.body;
+
+		user.password = bcrypt.hashSync(user.password, 10);
+
+		const newUser = await Users.add(user);
+		const token = generateToken(newUser);
+		return res.status(201).json(token);
+	} catch (err) {
+		res.status(500).json({
+			message: 'Sorry, but there was an error while creating that user'
+		});
+	}
 }
 
 function login(req, res) {
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const db = require('../database/dbConfig');
 
 module.exports = {
 	add,
+	findBy,
 	findById
 };
 
