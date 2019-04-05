d44c4e0db93d5d10e0dea656979af192ce830d83
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-05 09:15:51 -0700

    Create register route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+import Register from './components/Auth/Register';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Hello</h1>
+				<Route
+					path="/register"
+					render={props => <Register {...props} />}
+				/>
 			</div>
 		);
 	}
